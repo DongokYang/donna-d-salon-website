cb9614a0f4b7259b912734f60000735dafe31ad7
Sulnklm <hyeinsuin@icloud.com>
2024-12-24 10:01:54 -0800

    homepage customer reviews upload

--- donna-d-salon-website/src/components/home/Section4.jsx
+++ donna-d-salon-website/src/components/home/Section4.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import instagramAccount from "/public/image/home/instagram.png";
 
 function Section4() {
   return (
-    <div>
-      
+    <div className="bg-black bg-opacity-75 grid pb-[6rem]">
+      <div className="flex justify-between container mx-auto">
+        <div className="flex flex-col justify-center">
+          <h3 className="bg-gradient-to-r from-customGoldStart to-customGoldEnd bg-clip-text text-transparent">
+            Follow us
+          </h3>
+          <h2>Follow us on Instagram</h2>
+          <p>Explore our clients' stunning style makeovers</p>
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="text-white text-2xl mt-4"
+          />
+        </div>
+        <img
+          src={instagramAccount}
+          alt="Instagram account image"
+          className="max-w-[20rem] w-full h-auto mx-auto"
+        />
+      </div>
+      <div className="absolute flex bottom-0 right-0 left-0 translate-y-20">
+        <img
+          src="https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className=" border border-customGoldStart -translate-x-3 max-w-[15rem] w-full h-auto object-cover mx-auto"
+        />
+        <img
+          src="https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="border border-customGoldStart -translate-x-3 max-w-[15rem] w-full h-auto object-cover mx-auto"
+        />
+        <img
+          src="https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="border border-customGoldStart -translate-x-3 max-w-[15rem] w-full h-auto object-cover mx-auto"
+        />
+        <img
+          src="https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="border border-customGoldStart -translate-x-3 max-w-[15rem] w-full h-auto object-cover mx-auto"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Section4
+export default Section4;
--- donna-d-salon-website/tailwind.config.js
+++ donna-d-salon-website/tailwind.config.js
@@ -5,21 +5,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-<<<<<<< HEAD
-=======
-      rotate: {
-        2: "2deg",
-      },
->>>>>>> 60af804784ca66d8b22ecfbeb9db1c33247bd387
 
       colors: {
         customGoldStart: "#FFC74D",
         customGoldEnd: "#D98F20",
         customGrayDark: "#1D1D1D",
-<<<<<<< HEAD
-=======
-        customGold: "#DAA520",
->>>>>>> 60af804784ca66d8b22ecfbeb9db1c33247bd387
       },
 
       backgroundImage: {
@@ -37,7 +27,6 @@ module.exports = {
         "h1-md": ["40px", { lineHeight: "1.5" }],
         h2: ["23px", { lineHeight: "1.5" }],
         "h2-md": ["25px", { lineHeight: "1.5" }],
-<<<<<<< HEAD
         h3: ["38px", { lineHeight: "1.5" }],
         "h3-md": ["39px", { lineHeight: "1.5" }],
         "h3-lg": ["40px", { lineHeight: "1.5" }],
@@ -47,27 +36,13 @@ module.exports = {
         h5: ["20px", { lineHeight: "1.8" }],
         "h5-lg": ["20px", { lineHeight: "1.9" }],
         p: ["16px", {  }],
-=======
-        h3: ["25px", { lineHeight: "1.5" }],
-        "h3-md": ["30px", { lineHeight: "1.5" }],
-        "h3-lg": ["40px", { lineHeight: "1.5" }],
-
-        h4: ["16px", { lineHeight: "1.5" }],
-        "h4-md": ["17px", { lineHeight: "1.5" }],
-        "h4-lg": ["18px", { lineHeight: "1.5" }],
-        p: ["15px", { lineHeight: "1.8" }],
->>>>>>> 60af804784ca66d8b22ecfbeb9db1c33247bd387
         "p-md": ["16px", { lineHeight: "1.9" }],
         "p-lg": ["17px", { lineHeight: "1.9" }],
       },
     },
   },
   plugins: [
-<<<<<<< HEAD
     function ({ addComponents }) {
-=======
-    function ({ addComponents, addUtilities, theme }) {
->>>>>>> 60af804784ca66d8b22ecfbeb9db1c33247bd387
       addComponents({
         h1: {
           "@apply font-Aboreto text-h1 font-bold": {},
@@ -99,7 +74,6 @@ module.exports = {
             "@apply text-h4-lg": {},
           },
         },
-<<<<<<< HEAD
         h5: {
           "@apply font-Lato text-h5 font-[300] text-white": {},
           "@screen lg": {
@@ -111,13 +85,6 @@ module.exports = {
           "@screen md": {
             "@apply text-p-md": {},
           },
-=======
-        p: {
-          "@apply font-Lato text-p text-white font-[300]": {},
-          "@screen md": {
-            "@apply text-p-md": {},
-          },
->>>>>>> 60af804784ca66d8b22ecfbeb9db1c33247bd387
           "@screen lg": {
             "@apply text-p-lg": {},
           },
